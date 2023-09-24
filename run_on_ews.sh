dd8551a3ccb69ef4ad39d6225457cc500dd7c7f4
nebu <aditya.v.nebhrajani@gmail.com>
2023-09-24 15:06:43 -0500

    fix run_on_ews

--- run_on_ews.sh
+++ run_on_ews.sh
@@ -8,16 +8,17 @@
 #                especially for viewing waves (X forwarding/VNC is slow).
 #------------------------------------------------------------------------
 
-remote_server="netid@linux.ews.illinois.edu"
-remote_directory="~/sigarch_blank_rtl_project/hello-world"
+remote_server="avn5@linux.ews.illinois.edu"
+remote_directory="~/sigarch"
+current_project="hello-world"
 output_directory="./ews_run_artifacts"
 
 mkdir -p $output_directory
 
-rsync -avz . "$remote_server:$remote_directory" --exclude doc --exclude ews_run_artifacts --exclude .git
-ssh "$remote_server" "source ~/setup_synopsys_toolchain.sh && cd $remote_directory && make run"
-rsync -avz "$remote_server:$remote_directory/sim/dump.vcd" "$output_directory"
-rsync -avz "$remote_server:$remote_directory/sim/compile.log" "$output_directory"
-rsync -avz "$remote_server:$remote_directory/sim/simulation.log" "$output_directory"
+rsync -avz "./$current_project" "$remote_server:$remote_directory" --exclude doc --exclude ews_run_artifacts --exclude .git
+ssh "$remote_server" ". /etc/profile && source ~/setup_synopsys_toolchain.sh && cd $remote_directory/$current_project && make run"
+rsync -avz "$remote_server:$remote_directory/$current_project/sim/dump.vcd" "$output_directory"
+rsync -avz "$remote_server:$remote_directory/$current_project/sim/compile.log" "$output_directory"
+rsync -avz "$remote_server:$remote_directory/$current_project/sim/simulation.log" "$output_directory"
 
 echo "run_on_ews.sh: exiting"
